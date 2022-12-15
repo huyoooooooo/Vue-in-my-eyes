3d4929787c1f71a4bedd18b21e3307afba297950
richard <muyuxiaoke@163.com>
2022-12-15 14:33:42 +0800

    branch v2.5-demo-4.9

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.val }}</li>
+    </ul>
+    <button @click="change">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      items: [
+        {id: 0, val: 'A'},
+        {id: 1, val: 'B'},
+        {id: 2, val: 'C'},
+        {id: 3, val: 'D'}
+      ]
+    }
+  },
+  methods: {
+    change() {
+      this.items.reverse().push({ id: 5, val: 'F' })
+      this.items.splice(1, 1, {id: 4, val: 'E'})
+    }
+  }
+}
+</script>
